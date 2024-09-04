db331377f9bd787edea719123dc6b8214be32b60
= <kmohan0910@gmail.com>
2024-09-04 10:27:13 +0530

    Updated UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
@@ -15,9 +15,8 @@ export class AppComponent {
   title = 'api-caching';
   result: any;
   isLoading = false; // To track the loading state
-
+  data: any[] = [];
   constructor(private httpRequest: HttpClient) {}
-  i = 1;
   ngOnInit() {
     this.fetchData();
   }
@@ -26,23 +25,18 @@ export class AppComponent {
   fetchData() {
     this.isLoading = true; // Start loading
     this.httpRequest
-      .get('https://jsonplaceholder.typicode.com/todos/' + this.i)
-      .subscribe((res) => {
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((res: any) => {
         console.log(res);
-        this.result = res;
-        setTimeout(() => {
-          this.isLoading = false; // Stop loading
-        });
+        this.data = res;
+
+        this.isLoading = false; // Stop loading
       });
   }
 
   // Method to fetch new data
   updateData() {
-    this.i += 1;
-    this.fetchData();
-  }
-  lastData() {
-    this.i -= 1;
+    // this.i += 1;
     this.fetchData();
   }
 }
